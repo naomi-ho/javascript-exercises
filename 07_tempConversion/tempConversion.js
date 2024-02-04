8c0010c4659abf7ca8e145cab653b96680b2303a
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-02-04 21:00:24 +1100

    Create convertToCelsius and convertToFahrenheit functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let unRoundedCelsius = (fahrenheitTemp - 32) * (5/9);
+  let roundedCelsius = unRoundedCelsius.toFixed(1);
+  return Number(roundedCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let unRoundedFahrenheit = celsiusTemp * (9/5) + 32;
+  let roundedFahrenheit = unRoundedFahrenheit.toFixed(1);
+  return Number(roundedFahrenheit);
 };
 
 // Do not edit below this line
