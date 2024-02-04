7acae8b0555196b6a41226f3405680bc99338d28
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-02-04 14:56:39 +1100

    Create removeFromArray()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+    // create a new array
+    let newArray = [];
+    // checks each item in array
+    array.forEach((item) => {
+        // push every element into array unless it is included in values
+        if (!values.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    // return array with removed values
+    return newArray;
 };
 
 // Do not edit below this line
