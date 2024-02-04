4b2218aa12a2190e64dad949c5f5511ad5cd1217
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-02-04 18:56:20 +1100

    Create sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    let array = [];
+    let sum = 0;
+    let temp;
 
+    if (first < 0 || last < 0) return "ERROR";
+    if (typeof first !== "number" || typeof last !== "number") return "ERROR";
+    
+    if (first > last) {
+        temp = first;
+        first = last;
+        last = temp;
+    }
+
+    for (let i = first; i <= last; i++) {
+        array.push(i);
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum;
 };
 
 // Do not edit below this line
