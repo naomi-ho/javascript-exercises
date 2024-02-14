248021e3b381c06721e46f5c1c6f1f52e3ffd98c
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-02-14 18:48:53 +1100

    Add code to each function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return result = a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return result = a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+    result += array[i];
+  }
+	return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = array[0];
+  for (let i = 1; i < array.length; i++) {
+    result *= array[i];
+  }
+	return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+
+	return result;
 };
 
 // Do not edit below this line
