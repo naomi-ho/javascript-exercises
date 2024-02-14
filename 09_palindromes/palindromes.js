004b6f89029c9d14b75ffe35abd67e1378862734
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-02-14 21:13:43 +1100

    Add code to functions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // convert to lowercase
+    let lowerStr = str.toLowerCase();
+    console.log(lowerStr);
 
+    // remove numbers and special characters
+    const specialChar = /[^A-Za-z0-9]+/g;
+    let replacedStr = lowerStr.replace(specialChar, "");
+    console.log(replacedStr);
+
+    // reverse string
+    let reversedStr = replacedStr.split("").reverse().join("");
+
+    // check if both strings are equal to one another and return true or false
+    return replacedStr === reversedStr;
 };
 
 // Do not edit below this line
