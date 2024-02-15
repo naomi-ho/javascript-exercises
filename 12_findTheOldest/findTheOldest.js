e5a0fa0af52a196edb772b8fa14d04c9f7c57e8c
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-02-15 16:13:23 +1100

    Add code to function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((oldestPerson, currentPerson) => {
+        const oldest = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const current = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldest < current ? currentPerson : oldestPerson;
+    });
+};
 
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
